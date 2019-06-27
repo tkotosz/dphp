7f4e24a88877630906fc84d1d784c56f0e90a05f
Tibor Kotosz <tkotosz@inviqa.com>
2019-06-27 10:17:10 +0200

    Update dphp.sh

--- dphp.sh
+++ dphp.sh
@@ -9,7 +9,7 @@ function run() {
     if [ -z "$2" ]; then 
         docker run -ti --rm php:$1;
     else
-        docker run -ti --rm -v $(pwd):/this php:$1 -f this/$2;
+        docker run -ti --rm -v $(pwd):/this php:$1 php this/$2;
     fi
 
     printf "\n";
